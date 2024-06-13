0d6c7f9ff0b9dec913503fb2fc568625e4a067b8
Rimsha Zahid <122692816+IRims@users.noreply.github.com>
2024-06-13 10:00:51 +0500

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build','static')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -154,4 +154,4 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "static/img"), 
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
