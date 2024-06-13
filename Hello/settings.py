0234fabd671b065360c2a71749ba2052f8e2a9b1
Rimsha Zahid <122692816+IRims@users.noreply.github.com>
2024-06-13 10:03:01 +0500

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-lqw!+eb2vp+q!3fcd@t$n!4vfjc0o)+%0n2c#!6bdy20jh5rzn
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
